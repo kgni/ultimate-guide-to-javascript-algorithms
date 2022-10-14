8c126ec7e754f65654847135ab6a93a7c10718f9
kgni <admin@mdia.dk>
2022-10-14 11:19:27 +0200

    add solution reverseString

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -1,14 +1,49 @@
-/* CHALLENGE
-Given a string of text, write an algorithm that returns the text received in a reversed format. 
-E.g reverseString('algorithms') // should return 'smhtirogla'
-*/
-
-
+// CHAINING BUILT-IN METHODS
 
 function reverseString(text) {
-    // Code goes here
+	return text.split('').reverse().join('');
 }
 
+//  CHAINING BUILT-IN METHODS USING ES6
 
+function reverseString(text) {
+	return [...text].reverse().join('');
+}
 
-module.exports = reverseString
+// USING A FOR LOOP
+
+function reverseString(text) {
+	let result = '';
+	for (let i = text.length - 1; i >= 0; i--) {
+		result += text[i];
+	}
+	return result;
+}
+
+// USING A FOR..OF LOOP IN ES6
+
+function reverseString(text) {
+	let result = '';
+	for (let char of text) {
+		result = char + result;
+	}
+	return result;
+}
+
+// RECURSIVE METHOD
+
+function reverseString(text) {
+	if (text === '') {
+		return '';
+	} else {
+		return reverseString(text.substr(1)) + text[0];
+	}
+}
+
+// USING .REDUCE()
+
+function reverseString(text) {
+	return text.split('').reduce((acc, char) => char + acc);
+}
+
+module.exports = reverseString;
