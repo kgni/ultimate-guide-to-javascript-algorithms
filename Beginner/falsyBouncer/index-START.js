1ee0d9fea3ccd974f84e735b9cf5f27b6b440056
kgni <admin@mdia.dk>
2022-10-20 23:20:14 +0200

    add solution falsyBouncer

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -5,10 +5,8 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
-
 function falsyBouncer(array) {
-    // Code goes here
+	return array.filter((el) => el);
 }
 
-
-module.exports = falsyBouncer
+module.exports = falsyBouncer;
