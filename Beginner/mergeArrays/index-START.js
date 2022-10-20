fc8ab4c81fe424dc7fcfc839416e817fc0d22a14
kgni <admin@mdia.dk>
2022-10-20 23:14:58 +0200

    add solution mergeArrays

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,18 +4,33 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// using set to remove duplicates
 function mergeArrays(...arrays) {
+	let jointArray = [];
 
-    let jointArray = []
-    
-    arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
+	arrays.forEach((array) => {
+		jointArray = [...jointArray, ...array];
+	});
 
-    return [...new Set([...jointArray])]
-
-    
+	return [...new Set([...jointArray])];
 }
 
+// using filter to remove duplicates
 
-module.exports = mergeArrays
+function mergeArrays(...arrays) {
+	let jointArray = [];
+
+	arrays.forEach((array) => {
+		jointArray = [...jointArray, ...array];
+	});
+
+	const uniqueArray = jointArray.filter(
+		(item, index) => jointArray.indexOf(item) === index
+	);
+
+	return uniqueArray;
+}
+
+module.exports = mergeArrays;
+
+console.log([...new Set([1, 2, 3, 4])]);
