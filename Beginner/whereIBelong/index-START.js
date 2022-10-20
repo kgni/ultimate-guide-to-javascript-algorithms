8904c8bddee03da068f40b6edce7e5e4f74752a4
kgni <admin@mdia.dk>
2022-10-20 23:40:36 +0200

    add solution whereIBelong

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -7,13 +7,40 @@ whereIBelong([1,2,3,4], 1.5) // should return 1 because it is greater
 than 1(index 0), but less than 2(index 1).
 */
 
-
-
+// checking if the is less than or equal to the current item in array after sorting.
 function whereIBelong(arr, num) {
-   // Code goes here
+	// sort array, ascending.
 
+	const sortedArray = arr.sort((a, b) => a - b);
+
+	// loop through sorted array, check if the num is less than the current value, if it is, return the previous value.
+
+	for (let i = 0; i < sortedArray.length; i++) {
+		if (num <= sortedArray[i]) {
+			return i;
+		}
+	}
+
+	return arr.length;
 }
 
+// counting the smaller nums (fastest)
+function whereIBelong(arr, num) {
+	var counter = 0;
+	for (i = 0; i < arr.length; i++) {
+		if (num > arr[i]) {
+			counter++;
+		}
+	}
+	return counter;
+}
 
+// push, sort, indexOf
+function whereIBelong(arr, num) {
+	arr.push(num);
+	arr.sort((a, b) => a - b);
+	return arr.indexOf(num);
+}
 
-module.exports = whereIBelong
+console.log(whereIBelong([1, 2, 3, 4], 1.5));
+module.exports = whereIBelong;
